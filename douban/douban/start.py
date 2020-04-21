b3b91a75c9976302c104e3228361b7d5306fdd5c
Yuanqing <54049184+yuanqingZou@users.noreply.github.com>
2020-04-21 15:43:02 +0800

    Update start
    分开用于DB测试和Scrapy测试的启动代码

--- douban/douban/start.py
+++ douban/douban/start.py
@@ -1,4 +1,15 @@
 from scrapy import cmdline
-
-# use cmd in here
+#ScrapyTest
+# scrapy book_info where tag=科技
 cmdline.execute("scrapy crawl book -o books.json -a tag=科技".split())
+
+# scrapy comment_info where bookID=6709783
+# cmdline.execute("scrapy crawl comment -a bookID=6709783".split())
+
+
+#DBTest
+# scrapy book_info where tag=科技
+# cmdline.execute("scrapy crawl book -a tag=科技".split())
+
+# scrapy comment_info where bookID=6709783
+# cmdline.execute("scrapy crawl comment -a bookID=6709783".split())
