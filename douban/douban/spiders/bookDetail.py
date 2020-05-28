d1dee2bb95892fa5e940cb6733ccdef759869f8b
Minghou Lei <36237320+Mistletoer@users.noreply.github.com>
2020-05-29 02:10:17 +0800

    Fixed Book Detail Crawling: The script can now more intelligently determine whether the crawled item has been folded by Douban, ensuring that the entire content is crawled every time
    Signed-off-by: Minghou Lei <36237320+Mistletoer@users.noreply.github.com>

--- douban/douban/spiders/bookDetail.py
+++ douban/douban/spiders/bookDetail.py
@@ -11,7 +11,7 @@ class BookDetailSpider(scrapy.Spider):
     bookid = ''
 
     custom_settings = {
-        'ITEM_PIPELINES': {'douban.pipelines.BookDetailAsynPipeline': 100}
+        'ITEM_PIPELINES': {'douban.pipelines.BookDetailAsynPipeline': 300}
     }
 
     def __init__(self, bookID=None, *args, **kwargs):
@@ -22,17 +22,32 @@ class BookDetailSpider(scrapy.Spider):
 
     def parse(self, response):
         sel = scrapy.Selector(response)
+
+
+        try:
+            bookIntroduct = sel.xpath('//div[@id="link-report"]//*[@class="all hidden"]//div[@class="intro"]').xpath("string(.)").extract()[0].replace('\t', '')
+        except Exception as e:
+            print("book_introduct NO ADD-ON!")
+            bookIntroduct = sel.xpath('//div[@id="link-report"]//div[@class="intro"]').xpath("string(.)").extract()[0].replace('\t', '')
+
+        try:
+            authorIntroduct = sel.xpath('//div[@class="related_info"]//span[@class="all hidden "]/div[@class="intro"]').xpath('string(.)').extract()[0].replace('\t', '')
+        except Exception as e:
+            print("author_introduct NO ADD-ON!")
+            authorIntroduct = sel.xpath('//div[@class="related_info"]/div[@class="indent "]//div[@class="intro"]').xpath('string(.)').extract()[0].replace('\t', ''),
+
+
         try:
             item = BookDetail(
-                book_introduct = sel.xpath('//div[@id="link-report"]//div[@class="intro"]').xpath("string(.)").extract()[0].replace('...(展开全部)','').replace('\n', '').replace('\t', ''),
+                book_introduct = bookIntroduct,
 
-                table = sel.xpath('//*[@id="dir_'+self.bookid+'_full"]').xpath('string(.)').extract()[0].replace('\n', '').replace('· · · · · ·     (收起)','').replace('\t', ''),
+                table = sel.xpath('//*[@id="dir_'+self.bookid+'_full"]').xpath('string(.)').extract()[0].replace('· · · · · ·     (收起)','').replace('\t', ''),
 
-                author_introduct = sel.xpath('//div[@class="related_info"]//div[@class="intro"]').xpath('string(.)').extract()[0].replace('\n', '').replace('\t', ''),
+                author_introduct = authorIntroduct,
                 book_id = self.bookid
             )
             yield item
+            return
         except Exception as e:
-            print(e)
-            print("Yield BookDetail Error!")
+            print("Yield Book Detail Error!")
             pass
